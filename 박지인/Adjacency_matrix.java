e36009e4e91306be628a9af428765a038ad2ad9d
jeein2222 <jiinbag057@gmail.com>
2022-11-12 15:28:42 +0900

    dfs/bfs 문제 및 개념

---
+++ 박지인/Adjacency_matrix.java
@@ -0,0 +1,17 @@
+package 박지인;
+
+public class Adjacency_matrix {
+    public static void main(String[] args) {
+        int INF = 999999999;
+
+        int[][] graph={
+            {0,7,5},
+            {7,0,INF},
+            {5,INF,0}
+        };
+
+        System.out.println(graph);
+
+    }
+    
+}
---
+++ 박지인/pracQueue.java
@@ -0,0 +1,19 @@
+package 박지인;
+
+import java.util.*;
+public class pracQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q);//[1,2,3]
+
+        q.poll();
+        System.out.println(q);//[2,3]
+
+        q.clear();
+        System.out.println(q);//[]
+    }
+}
